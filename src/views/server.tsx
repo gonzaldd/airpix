c3e9726e5856a80e5c1cbdbf264922037397f827
Gonzalo <gonzalodedomingo@gmail.com>
2025-01-23 20:30:56 -0300

    Add netinfo

--- src/views/server.tsx
+++ src/views/server.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Image,
   View,
@@ -6,9 +6,11 @@ import {
   Dimensions,
   Modal,
   TouchableOpacity,
+  Text,
 } from 'react-native';
 import { launchImageLibrary } from 'react-native-image-picker';
 import { Button, ButtonText, ButtonGroup } from '@/components/ui/button';
+import NetInfo from '@react-native-community/netinfo';
 
 import useTcpServer from '../hooks/socketServer';
 import Gallery from './gallery';
@@ -21,6 +23,7 @@ const ServerView = () => {
   const [images, setImages] = useState<any>([]);
   const [selectedImage, setSelectedImage] = useState<ImagePicker | null>(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [deviceIp, setDeviceIp] = useState<string | null>(null)
 
   const onClientConnect = () => {
     setTimeout(() => {
@@ -52,9 +55,18 @@ const ServerView = () => {
     sendMessage(result.assets?.map(itm => itm.base64));
   };
 
+  useEffect(() => {
+    // getIpAddress().then((ip) => setDeviceIp(ip));
+    NetInfo.fetch().then(state => {
+      const ipAddress = (state?.details as { ipAddress: string })?.ipAddress;
+      setDeviceIp(ipAddress);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      {/* <Text>IP del servidor: {ip}</Text> */}
+      <Text style={styles.ipTitle}>IP para el cliente:</Text>
+      <Text style={[styles.ipText, styles.pb10]}>{deviceIp}</Text>
       {images?.length > 0 && <Gallery
         data={images}
         keyExtractor={(item: any) => item.uri}
@@ -113,6 +125,17 @@ const styles = StyleSheet.create({
     width: Dimensions.get('window').width * 0.9,
     height: Dimensions.get('window').width * 0.9,
     resizeMode: 'contain',
+  },
+  ipTitle: {
+    fontSize: 14,
+    fontWeight: 700,
+  },
+  ipText: {
+    fontSize: 19,
+    fontWeight: 700,
+  },
+  pb10: {
+    paddingBottom: 10,
   },
 });
 
